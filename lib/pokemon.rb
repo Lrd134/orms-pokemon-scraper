3b52190370a1db8b2cfce30676b1f11d0e13519d
Lrd134 <larryc3200@gmail.com>
2021-04-27 17:09:15 -0400

    commit tree bc7147101e29412d34ece389b769cdd043b98f40

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,2 +1,28 @@
 class Pokemon
+    attr_accessor :name, :type, :id, :db
+    def initialize(name:, type:, id:, db:)
+        # binding.pry
+        @name = name
+        @id = id
+        @type = type
+        @db = db
+    end
+    def self.save(name, type, db)
+        # binding.pry
+        sql = <<-SQL
+            INSERT INTO pokemon (name, type) VALUES (?, ?)
+        SQL
+        db.execute(sql, name, type)
+    end
+    def self.find(id, db)
+        # binding.pry
+        sql = <<-SQL
+        SELECT * FROM pokemon
+        WHERE id == ?
+        SQL
+        found = db.execute(sql, id).flatten
+        new_poke = Pokemon.new(name:found[1], type:found[2], id:found[0], db:db)
+
+        new_poke
+    end
 end
